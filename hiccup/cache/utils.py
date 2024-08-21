2d28caca1da42750d2c32c6d043b0d4b3b592bc2
DarcJC <me@darc.pro>
2024-08-21 17:53:12 +0800

    Add permission cache

--- hiccup/cache/utils.py
+++ hiccup/cache/utils.py
@@ -1,12 +1,50 @@
 from datetime import timedelta
+from typing import Optional
+
+from sqlalchemy import select
+from sqlalchemy.orm import joinedload
 
 from hiccup.cache import AsyncRedisSessionLocal
+from hiccup.db import AsyncSessionLocal
+from hiccup.db.permission import PermissionGroup
+from hiccup.db.user import ClassicIdentify
 
 PREFIX_MAP = {
-    "nonce": 'NONCE-',
+    "nonce": 'NONCE::',
+    "user-permission": 'USER-PERMISSION::'
 }
 
 
 async def cache_nonce(nonce: str, expire_in: timedelta = timedelta(minutes=5)) -> bool:
     async with AsyncRedisSessionLocal() as session:
         return await session.set(f'{PREFIX_MAP["nonce"]}{nonce}', 1, ex=expire_in, nx=True)
+
+
+async def invalidate_permission_cache(uid: int) -> None:
+    async with AsyncRedisSessionLocal() as session:
+        await session.delete(f'{PREFIX_MAP["user-permission"]}{uid}')
+
+
+async def get_user_permission_no_cache(uid: int) -> Optional[set[str]]:
+    async with AsyncSessionLocal() as session:
+        db_user: Optional[ClassicIdentify] = await session.scalar(select(ClassicIdentify).options(joinedload(ClassicIdentify.permission_groups)).where(ClassicIdentify.id == uid).limit(1))
+        if db_user is None:
+            return None
+        permissions: set[str] = {p for p in db_user.permissions}
+        if permissions is None:
+            permissions = set()
+
+        for group in db_user.permission_groups:
+            group: PermissionGroup
+            permissions.union( { p for p in group.permissions } )
+
+        return permissions
+
+
+async def get_user_permission_cached(uid: int) -> set[str]:
+    async with AsyncRedisSessionLocal() as session:
+        value: list[bytes] = await session.lrange(f'{PREFIX_MAP["user-permission"]}{uid}', 0, -1)
+        if len(value) >= 0:
+            return { p.decode('utf-8') for p in value }
+
+    pass
--- hiccup/captcha/__init__.py
+++ hiccup/captcha/__init__.py
@@ -1,11 +1,14 @@
-from typing import Any, Union, Awaitable
+from typing import Any, Union, Awaitable, Optional
 
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.requests import Request
 from starlette.websockets import WebSocket
 from strawberry import BasePermission, Info
 
 from hiccup import SETTINGS
 from hiccup.captcha.turnstile import Turnstile
+from hiccup.db import AuthToken
 
 
 class IsPassedCaptcha(BasePermission):
@@ -20,6 +23,23 @@ class IsPassedCaptcha(BasePermission):
         request: Union[Request, WebSocket] = info.context["request"]
         if "X-Hiccup-Captcha" in request.headers:
             return await Turnstile(secret_key=SETTINGS.captcha_turnstile_secret).verify(request.headers.get("X-Hiccup-Captcha"))
+
+        return False
+
+
+class HasPermission(BasePermission):
+    message = "Access denied"
+
+    async def has_permission(
+            self, source: Any, info: Info, **kwargs: Any
+    ) -> bool:
+        request: Union[Request, WebSocket] = info.context["request"]
+        if "X-Hiccup-Token" in request.headers:
+            access_token = request.headers.get("X-Hiccup-Token")
+            async with AsyncSession() as session:
+                db_token: Optional[AuthToken] = (await session.scalars(select(AuthToken).where(AuthToken.token == access_token).limit(1))).one_or_none()
+                if db_token is not None:
+                    db_token.classic_user_id
 
         return False
 
